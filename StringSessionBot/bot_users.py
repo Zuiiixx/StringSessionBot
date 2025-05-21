fe8293a272b6a7f3eb033bad9fa14a1a1da6de02
Zuiiixx <b7ctx5d84qx@gmail.com>
2025-05-21 09:29:38 +0530

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -1,30 +1,23 @@
 from pyrogram.types import Message
 from pyrogram import Client, filters
-
 from env import DATABASE_URL
 
-
 if DATABASE_URL != '':
-    from StringSessionBot.database import SESSION
-    from StringSessionBot.database.users_sql import Users, num_users
-
+    from StringSessionBot.database import db
 
 @Client.on_message(~filters.service, group=1)
 async def users_sql(_, msg: Message):
     if DATABASE_URL == '':
         return
     if msg.from_user:
-        q = SESSION.query(Users).get(int(msg.from_user.id))
-        if not q:
-            SESSION.add(Users(msg.from_user.id))
-            SESSION.commit()
-        else:
-            SESSION.close()
-
+        user_id = msg.from_user.id
+        existing_user = await db["users"].find_one({"user_id": user_id})
+        if not existing_user:
+            await db["users"].insert_one({"user_id": user_id})
 
 @Client.on_message(filters.user(1946995626) & filters.command("stats"))
 async def _stats(_, msg: Message):
     if DATABASE_URL == '':
         return
-    users = await num_users()
+    users = await db["users"].count_documents({})
     await msg.reply(f"Total Users : {users}", quote=True)
