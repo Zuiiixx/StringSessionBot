854a9c7ad2e4818e6e8b85d879f7618c477d2e4a
Zuiiixx <b7ctx5d84qx@gmail.com>
2025-05-21 09:27:17 +0530

    Update __init__.py

--- StringSessionBot/database/__init__.py
+++ StringSessionBot/database/__init__.py
@@ -1,21 +1,9 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
+from motor.motor_asyncio import AsyncIOMotorClient
 from env import DATABASE_URL
 
-count_ = 0
-def start() -> scoped_session:
-    if DATABASE_URL == "":
-        if count_ < 1:
-            count += 1
-            return print("Database url not provided..\nBut this time I won't stop 😉")
-        return
-    engine = create_engine(DATABASE_URL)
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+client = None
+db = None
 
 if DATABASE_URL != "":
-    BASE = declarative_base()
-    SESSION = start()
+    client = AsyncIOMotorClient(DATABASE_URL)
+    db = client["stringsession"]  # change name as needed
