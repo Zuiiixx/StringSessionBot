79972462eebc5649397487ac3163e1fd4b3f617f
Zuiiixx <b7ctx5d84qx@gmail.com>
2025-06-22 20:38:24 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()  # MongoDB URL
+API_ID = os.getenv("API_ID", "24249014").strip()
+API_HASH = os.getenv("API_HASH", "0a8d55a05546869565d1c1ab07e2234f").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7742667419:AAEvIkYBCr1qHVslZVT4fkazgcxgqK5ZX9s").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://zuiiixx:zuiiixx@cluster0.jsrns5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").strip()  # MongoDB URL
 MUST_JOIN = os.getenv("MUST_JOIN", "")
 
 if not API_ID:
